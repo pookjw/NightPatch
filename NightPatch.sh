474c185ebaf57e2ac080e9c64b1f457edafb2d5a
Kim Jinwoo <kidjinwoo@me.com>
2017-05-10 16:06:38 +0900

    Version 90
    Added -customBuild option.

--- NightPatch.sh
+++ NightPatch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION=89
+VERSION=90
 BUILD=
 
 if [[ "${1}" == help || "${1}" == "-help" || "${1}" == "--help" ]]; then
@@ -33,7 +33,7 @@ function removeTmp(){
 
 function revertAll(){
 	if [[ -f /Library/NightPatch/NightPatchBuild ]]; then
-		if [[ "$(cat /Library/NightPatch/NightPatchBuild)" == "$(sw_vers -buildVersion)" ]]; then
+		if [[ "$(cat /Library/NightPatch/NightPatchBuild)" == "${SYSTEM_BUILD}" ]]; then
 			if [[ ! "${1}" == "-doNotPrint" && ! "${2}" == "-doNotPrint" ]]; then
 				applyLightCyan
 				echo "Reverting..."
@@ -64,7 +64,7 @@ function revertAll(){
 			fi
 		fi
 	else
-		if [[ -f "combo/url-$(sw_vers -buildVersion).txt" ]]; then
+		if [[ -f "combo/url-${SYSTEM_BUILD}.txt" ]]; then
 			showLines "*"
 			applyRed
 			echo "ERROR : No backup."
@@ -86,7 +86,7 @@ function revertAll(){
 }
 
 function revertUsingCombo(){
-	if [[ -f "combo/url-$(sw_vers -buildVersion).txt" ]]; then
+	if [[ -f "combo/url-${SYSTEM_BUILD}.txt" ]]; then
 		if [[ ! -d "$(xcode-select -p)" ]]; then
 			applyRed
 			echo "ERROR : Requires Command Line Tool. Enter 'xcode-select --install' command to install this."
@@ -108,18 +108,18 @@ function revertUsingCombo(){
 		fi
 		if [[ ! "${skipCheckSHA}" == YES ]]; then
 			echo "Checking downloaded file..."
-			if [[ ! -f "combo/sha-$(sw_vers -buildVersion).txt" ]]; then
+			if [[ ! -f "combo/sha-${SYSTEM_BUILD}.txt" ]]; then
 				applyRed
-				echo "ERROR : I can't find combo/sha-$(sw_vers -buildVersion).txt file."
+				echo "ERROR : I can't find combo/sha-${SYSTEM_BUILD}.txt file."
 				applyNoColor
 				quitTool1
 			fi
-			if [[ ! "$(shasum /tmp/update.dmg | awk '{ print $1 }')" == "$(cat "combo/sha-$(sw_vers -buildVersion).txt")" ]]; then
+			if [[ ! "$(shasum /tmp/update.dmg | awk '{ print $1 }')" == "$(cat "combo/sha-${SYSTEM_BUILD}.txt")" ]]; then
 				applyRed
 				echo "ERROR : Downloaded file is wrong. Downloading again..."
 				applyNoColor
 				downloadCombo
-				if [[ ! "$(shasum /tmp/update.dmg | awk '{ print $1 }')" == "$(cat "combo/sha-$(sw_vers -buildVersion).txt")" ]]; then
+				if [[ ! "$(shasum /tmp/update.dmg | awk '{ print $1 }')" == "$(cat "combo/sha-${SYSTEM_BUILD}.txt")" ]]; then
 					applyRed
 					echo "ERROR : SHA not matching."
 					quitTool1
@@ -185,29 +185,29 @@ function revertUsingCombo(){
 		if [[ -f /tmp/NightPatchBuild ]]; then
 			sudo rm /tmp/NightPatchBuild
 		fi
-		echo $(sw_vers -buildVersion) >> /tmp/NightPatchBuild
+		echo "${SYSTEM_BUILD}" >> /tmp/NightPatchBuild
 		sudo mv /tmp/NightPatchBuild /Library/NightPatch
 		echo "Done. Reverting from backup..."
 	else
 		applyRed
-		echo "ERROR : Your macOS is not supported. ($(sw_vers -buildVersion))"
+		echo "ERROR : Your macOS is not supported. (${SYSTEM_BUILD})"
 		quitTool1
 	fi
 }
 
 function downloadCombo(){
-	if [[ ! -f "combo/url-$(sw_vers -buildVersion).txt" ]]; then
+	if [[ ! -f "combo/url-${SYSTEM_BUILD}.txt" ]]; then
 		applyRed
-		echo "ERROR : I can't find combo/url-$(sw_vers -buildVersion).txt file."
+		echo "ERROR : I can't find combo/url-${SYSTEM_BUILD}.txt file."
 		quitTool1
 	fi
-	if [[ -z "$(cat "combo/url-$(sw_vers -buildVersion).txt")" ]]; then
+	if [[ -z "$(cat "combo/url-${SYSTEM_BUILD}.txt")" ]]; then
 		applyRed
-		echo "ERROR : combo/url-$(sw_vers -buildVersion).txt is wrong."
+		echo "ERROR : combo/url-${SYSTEM_BUILD}.txt is wrong."
 		quitTool1
 	fi
 	echo "Downloading update... (takes a few minutes)"
-	curl -o /tmp/update.dmg "$(cat "combo/url-$(sw_vers -buildVersion).txt")"
+	curl -o /tmp/update.dmg "$(cat "combo/url-${SYSTEM_BUILD}.txt")"
 	if [[ ! -f /tmp/update.dmg ]]; then
 		applyRed
 		echo "ERROR : Failed to download file."
@@ -288,15 +288,15 @@ function moveOldBackup(){
 }
 
 function checkSHA(){
-	if [[ -f "sha/sha-$(sw_vers -buildVersion)_${1}.txt" ]]; then
-		if [[ ! "$(cat "sha/sha-$(sw_vers -buildVersion)_${1}.txt")" == "$(shasum /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness | awk '{ print $1 }')" ]]; then
+	if [[ -f "sha/sha-${SYSTEM_BUILD}_${1}.txt" ]]; then
+		if [[ ! "$(cat "sha/sha-${SYSTEM_BUILD}_${1}.txt")" == "$(shasum /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness | awk '{ print $1 }')" ]]; then
 			applyNoColor
 			showLines "*"
 			applyRed
-			echo "ERROR : SHA not matching. Patch was failed. ($(sw_vers -buildVersion)-${1}-$(shasum /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness | awk '{ print $1 }'))"
+			echo "ERROR : SHA not matching. Patch was failed. (${SYSTEM_BUILD}-${1}-$(shasum /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness | awk '{ print $1 }'))"
 			applyNoColor
 			echo "Seems like your macOS system file was damaged or already patched by other tool."
-			if [[ -f "combo/url-$(sw_vers -buildVersion).txt" ]]; then
+			if [[ -f "combo/url-${SYSTEM_BUILD}.txt" ]]; then
 				echo "Try this command to repair system file. \033[1;31mDo not type $\033[0m. (takes a few minutes)"
 				echo
 				showCommandGuide "-revert combo"
@@ -331,6 +331,145 @@ function showCommandGuide(){
 		echo "\033[1;31m$\033[0m cd /tmp; curl -o NightPatch.zip https://codeload.github.com/pookjw/NightPatch/zip/master; unzip -o NightPatch.zip; cd NightPatch-master; chmod +x NightPatch.sh; ./NightPatch.sh ${1}"
 	else
 		echo "\033[1;31m$\033[0m ./NightPatch.sh ${1}"
+	fi
+}
+
+function checkSystem(){
+	applyRed
+	if [[ "$(sw_vers -productVersion | cut -d"." -f2)" -lt 12 ]]; then
+		MACOS_ERROR=YES
+	elif [[ "$(sw_vers -productVersion | cut -d"." -f2)" == 12 ]]; then
+		if [[ "$(sw_vers -productVersion | cut -d"." -f3)" -lt 4 ]]; then
+			MACOS_ERROR=YES
+		fi
+	fi
+	if [[ "${MACOS_ERROR}" == YES ]]; then
+		echo "ERROR : Requires macOS 10.12.4 or higher. (Detected version : $(sw_vers -productVersion))"
+		quitTool1
+	fi
+	if [[ ! "$(csrutil status)" == "System Integrity Protection status: disabled." ]]; then
+		echo "ERROR : Turn off System Integrity Protection before doing this."
+		quitTool1
+	fi
+	applyNoColor
+}
+
+function patchSystem(){
+	applyRed
+	if [[ ! -d patch ]]; then
+		echo "ERROR : I can't find patch folder. Try again."
+		quitTool1
+	fi
+	if [[ ! -f "patch/${SYSTEM_BUILD}.patch" ]]; then
+		echo "ERROR : I can't find patch/${SYSTEM_BUILD}.patch file. (seems like not supported macOS)"
+		quitTool1
+	fi
+	applyNoColor
+	sudo touch /System/test
+	if [[ ! -f /System/test ]]; then
+		applyRed
+		echo "ERROR : Can't write a file to root."
+		quitTool1
+	fi
+	sudo rm /System/test
+	if [[ -f /Library/NightPatch/NightPatchBuild ]]; then
+		if [[ "$(cat /Library/NightPatch/NightPatchBuild)" == "${SYSTEM_BUILD}" ]]; then
+			echo "Detected backup. Reverting..."
+			revertAll -doNotQuit -doNotPrint
+			echo "Patching again..."
+		fi
+	fi
+	if [[ -d /Library/NightPatch ]]; then
+		sudo rm -rf /Library/NightPatch
+	fi
+	sudo mkdir /Library/NightPatch
+	if [[ -f /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness-patch ]]; then
+		sudo rm /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness-patch
+	fi
+	applyRed
+	sudo cp /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness /Library/NightPatch/CoreBrightness.bak
+	sudo cp -r /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/_CodeSignature /Library/NightPatch/_CodeSignature.bak
+	if [[ -f /tmp/NightPatchBuild ]]; then
+		sudo rm /tmp/NightPatchBuild
+	fi
+	echo "${SYSTEM_BUILD}" >> /tmp/NightPatchBuild
+	sudo mv /tmp/NightPatchBuild /Library/NightPatch
+	applyPurple
+	sudo codesign -f -s - /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
+	applyRed
+	if [[ ! "${skipCheckSHA}" == YES ]]; then
+		checkSHA original
+	fi
+	sudo bspatch /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness-patch "patch/${SYSTEM_BUILD}.patch"
+	sudo rm /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
+	sudo mv /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness-patch /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
+	sudo chmod +x /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
+	applyPurple
+	sudo codesign -f -s - /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
+	applyRed
+	if [[ ! "${skipCheckSHA}" == YES ]]; then
+		checkSHA patched
+	fi
+	applyNoColor
+	echo "Backup was saved on \033[1;36m/Library/NightPatch\033[0m."
+	echo "Patch was done. Please reboot your Mac to complete."
+	quitTool0
+}
+
+function setToolMode(){
+	mode=patch
+	if [[ "${1}" == "-skipCheckSHA" || "${2}" == "-skipCheckSHA" || "${3}" == "-skipCheckSHA" || "${4}" == "-skipCheckSHA" || "${5}" == "-skipCheckSHA" || "${6}" == "-skipCheckSHA" || "${7}" == "-skipCheckSHA" || "${8}" == "-skipCheckSHA" || "${9}" == "-skipCheckSHA" ]]; then
+		echo "skipCheckSHA=\033[1;36mYES\033[0m"
+		skipCheckSHA=YES
+	fi
+	if [[ "${1}" == "-customBuild" || "${2}" == "-customBuild" || "${3}" == "-customBuild" || "${4}" == "-customBuild" || "${5}" == "-customBuild" || "${6}" == "-customBuild" || "${7}" == "-customBuild" || "${8}" == "-customBuild" || "${9}" == "-customBuild" ]]; then
+		echo "customBuild=\033[1;36mYES\033[0m"
+		customBuild=YES
+	fi
+	if [[ "${1}" == "-moveOldBackup" || "${2}" == "-moveOldBackup" || "${3}" == "-moveOldBackup" || "${4}" == "-moveOldBackup" || "${5}" == "-moveOldBackup" || "${6}" == "-moveOldBackup" || "${7}" == "-moveOldBackup" || "${8}" == "-moveOldBackup" || "${9}" == "-moveOldBackup" ]]; then
+		echo "mode=\033[1;36mmoveOldBackup\033[0m"
+		mode=moveOldBackup
+	fi
+	if [[ "${1}" == "-revert" || "${2}" == "-revert" || "${3}" == "-revert" || "${4}" == "-revert" || "${5}" == "-revert" || "${6}" == "-revert" || "${7}" == "-revert" || "${8}" == "-revert" || "${9}" == "-revert" ]]; then
+		if [[ "${1}" == "combo" || "${2}" == "combo" || "${3}" == "combo" || "${4}" == "combo" || "${5}" == "combo" || "${6}" == "combo" || "${7}" == "combo" || "${8}" == "combo" || "${9}" == "combo" ]]; then
+			echo "mode=\033[1;36mrevert \033[1;35m(combo)\033[0m"
+			mode=revertUsingCombo
+		else
+			echo "mode=\033[1;36mrevert\033[0m"
+			mode=revert
+		fi
+	fi
+	if [[ "${mode}" == patch ]]; then
+		echo "mode=\033[1;36mpatch\033[0m"
+	fi
+}
+
+function setBuild(){
+	if [[ "${customBuild}" == YES ]]; then
+		echo "Enter custom system build. (ex: 16E195)"
+		while(true); do
+			read -p "- " SYSTEM_BUILD
+			if [[ "${SYSTEM_BUILD}" == exit ]]; then
+				quitTool0
+			elif [[ ! -z "${SYSTEM_BUILD}" ]]; then
+				echo "SYSTEM_BUILD=\033[1;36m${SYSTEM_BUILD}\033[0m"
+				break
+			fi
+		done
+	else
+		SYSTEM_BUILD="$(sw_vers -buildVersion)"
+	fi
+}
+
+function showInitialMessage(){
+	showLines "*"
+	echo "NightPatch.sh by @pookjw. Version : ${VERSION}"
+	showLines "*"
+	if [[ "${BUILD}" == beta ]]; then
+		echo "**WARNING : This is beta version. I don't guarantee of any problems."
+		applyLightCyan
+		read -s -n 1 -p "Press any key to continue..."
+		applyNoColor
 	fi
 }
 
@@ -374,117 +513,23 @@ function quitTool1(){
 	exit 1
 }
 
-showLines "*"
-echo "NightPatch.sh by @pookjw. Version : ${VERSION}"
-showLines "*"
-if [[ "${BUILD}" == beta ]]; then
-	echo "**WARNING : This is beta version. I don't guarantee of any problems."
-	applyLightCyan
-	read -s -n 1 -p "Press any key to continue..."
-	applyNoColor
-fi
-if [[ "${1}" == "-skipCheckSHA" || "${2}" == "-skipCheckSHA" || "${3}" == "-skipCheckSHA" || "${4}" == "-skipCheckSHA" || "${5}" == "-skipCheckSHA" || "${6}" == "-skipCheckSHA" || "${7}" == "-skipCheckSHA" || "${8}" == "-skipCheckSHA" || "${9}" == "-skipCheckSHA" ]]; then
-	applyPurple
-	echo "skipCheckSHA=YES"
-	applyNoColor
-	skipCheckSHA=YES
-fi
-applyRed
-if [[ "$(sw_vers -productVersion | cut -d"." -f2)" -lt 12 ]]; then
-	MACOS_ERROR=YES
-elif [[ "$(sw_vers -productVersion | cut -d"." -f2)" == 12 ]]; then
-	if [[ "$(sw_vers -productVersion | cut -d"." -f3)" -lt 4 ]]; then
-		MACOS_ERROR=YES
-	fi
-fi
-if [[ "${MACOS_ERROR}" == YES ]]; then
-	echo "ERROR : Requires macOS 10.12.4 or higher. (Detected version : $(sw_vers -productVersion))"
-	quitTool1
-fi
-if [[ ! "$(csrutil status)" == "System Integrity Protection status: disabled." ]]; then
-	echo "ERROR : Turn off System Integrity Protection before doing this."
-	quitTool1
-fi
-if [[ "${1}" == "-moveOldBackup" || "${2}" == "-moveOldBackup" || "${3}" == "-moveOldBackup" || "${4}" == "-moveOldBackup" || "${5}" == "-moveOldBackup" || "${6}" == "-moveOldBackup" || "${7}" == "-moveOldBackup" || "${8}" == "-moveOldBackup" || "${9}" == "-moveOldBackup" ]]; then
-	applyLightCyan
-	echo "mode : moveOldBackup"
-	applyNoColor
+#########################################################################
+
+showInitialMessage
+checkSystem
+setToolMode "${1}" "${2}" "${3}" "${4}" "${5}" "${6}" "${7}" "${8}" "${9}"
+echo
+setBuild
+if [[ "${mode}" == moveOldBackup ]]; then
 	moveOldBackup
 	quitTool0
 fi
 moveOldBackup
-if [[ "${1}" == "-revert" || "${2}" == "-revert" || "${3}" == "-revert" || "${4}" == "-revert" || "${5}" == "-revert" || "${6}" == "-revert" || "${7}" == "-revert" || "${8}" == "-revert" || "${9}" == "-revert" ]]; then
-	if [[ "${1}" == "combo" || "${2}" == "combo" || "${3}" == "combo" || "${4}" == "combo" || "${5}" == "combo" || "${6}" == "combo" || "${7}" == "combo" || "${8}" == "combo" || "${9}" == "combo" ]]; then
-		applyLightCyan
-		echo "mode : revertUsingCombo"
-		applyNoColor
-		revertUsingCombo
-	else
-		applyLightCyan
-		echo "mode : revert"
-		applyNoColor
-	fi
+if [[ "${mode}" == revertUsingCombo ]]; then
+	revertUsingCombo
 	revertAll -rebootMessage
+elif [[ "${mode}" == revert ]]; then
+	revertAll -rebootMessage
+elif [[ "${mode}" == patch ]]; then
+	patchSystem
 fi
-applyLightCyan
-echo "mode : patch"
-applyNoColor
-applyRed
-if [[ ! -d patch ]]; then
-	echo "ERROR : I can't find patch folder. Try again."
-	quitTool1
-fi
-if [[ ! -f "patch/$(sw_vers -buildVersion).patch" ]]; then
-	echo "ERROR : I can't find patch/$(sw_vers -buildVersion).patch file. (seems like not supported macOS)"
-	quitTool1
-fi
-applyNoColor
-sudo touch /System/test
-if [[ ! -f /System/test ]]; then
-	applyRed
-	echo "ERROR : Can't write a file to root."
-	quitTool1
-fi
-sudo rm /System/test
-if [[ -f /Library/NightPatch/NightPatchBuild ]]; then
-	if [[ "$(cat /Library/NightPatch/NightPatchBuild)" == "$(sw_vers -buildVersion)" ]]; then
-		echo "Detected backup. Reverting..."
-		revertAll -doNotQuit -doNotPrint
-		echo "Patching again..."
-	fi
-fi
-if [[ -d /Library/NightPatch ]]; then
-	sudo rm -rf /Library/NightPatch
-fi
-sudo mkdir /Library/NightPatch
-if [[ -f /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness-patch ]]; then
-	sudo rm /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness-patch
-fi
-applyRed
-sudo cp /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness /Library/NightPatch/CoreBrightness.bak
-sudo cp -r /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/_CodeSignature /Library/NightPatch/_CodeSignature.bak
-if [[ -f /tmp/NightPatchBuild ]]; then
-	sudo rm /tmp/NightPatchBuild
-fi
-echo $(sw_vers -buildVersion) >> /tmp/NightPatchBuild
-sudo mv /tmp/NightPatchBuild /Library/NightPatch
-applyPurple
-sudo codesign -f -s - /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
-applyRed
-if [[ ! "${skipCheckSHA}" == YES ]]; then
-	checkSHA original
-fi
-sudo bspatch /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness-patch patch/$(sw_vers -buildVersion).patch
-sudo rm /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
-sudo mv /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness-patch /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
-sudo chmod +x /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
-applyPurple
-sudo codesign -f -s - /System/Library/PrivateFrameworks/CoreBrightness.framework/Versions/A/CoreBrightness
-applyRed
-if [[ ! "${skipCheckSHA}" == YES ]]; then
-	checkSHA patched
-fi
-applyNoColor
-echo "Backup was saved on \033[1;36m/Library/NightPatch\033[0m."
-echo "Patch was done. Please reboot your Mac to complete."
-quitTool0
